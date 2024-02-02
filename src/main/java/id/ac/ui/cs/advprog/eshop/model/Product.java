047bc92e2449254087b4ac156f24271d6ee37061
nabielsubhan <mnsubhan1509@gmail.com>
2024-02-02 16:35:25 +0700

    fixed string -> String

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter @Setter
 public class Product {
-    private string productID;
-    private string productName;
+    private String productID;
+    private String productName;
     private int productQuantity;
 }
