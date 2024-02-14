2f7ee3249b31e07853bc3efb8c1ee04bc9662607
nabielsubhan <mnsubhan1509@gmail.com>
2024-02-14 12:15:54 +0700

    add contexLoads test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,21 @@
 package id.ac.ui.cs.advprog.eshop;
 
+import id.ac.ui.cs.advprog.eshop.controller.ProductController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+	@Autowired
+	private ProductController controller;
 	@Test
 	void contextLoads() {
+		EshopApplication.main(new String[] {});
+		assertThat(controller).isNotNull();
 	}
 
 }
