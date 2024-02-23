848b3d7afd0a0ec3c1fc67afb209eb1247b1ba17
nabielsubhan <mnsubhan1509@gmail.com>
2024-02-24 00:36:48 +0700

    fix Autowired issue in EshopApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop;
 
-import id.ac.ui.cs.advprog.eshop.controller.ProductController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class EshopApplicationTests {
 
 	@Autowired
-	private ProductController controller;
+	private EshopApplication eshopApplication;
 	@Test
 	void contextLoads() {
 		EshopApplication.main(new String[] {});
-		assertThat(controller).isNotNull();
+		assertThat(eshopApplication).isNotNull();
 	}
 
 }
