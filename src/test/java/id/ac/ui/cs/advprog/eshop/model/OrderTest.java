bed237675ae3a6279bb151c904f0168828fbb277
nabielsubhan <mnsubhan1509@gmail.com>
2024-03-05 10:46:58 +0700

    add import for assertions

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
     private List<Product> products;
